596b67aa3a29bcb4c58bb6b9c070a356498a6199
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-01-29 13:37:53 +0100

    Designed the models and controllers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+
+
+  include CommentsHelper
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = 1
+    @comment.post_id = params[:post_id]
+
+    @comment.save
+    redirect_to post_path(@comment.post)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,38 @@
 class UsersController < ApplicationController
+  include UsersHelper
   def index
-    
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @post = Post.new
+    @post.user_id = @user.id
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to :action => "index"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 end
